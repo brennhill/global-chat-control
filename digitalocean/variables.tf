10bfbb1e0e8f3bc904e913613132d2d30c0fb3d5
Brenn <brenn@dontbugme.com>
2026-02-28 10:23:26 +0100

    Add terraform.tfvars.example files for all providers

--- digitalocean/variables.tf
+++ digitalocean/variables.tf
@@ -1,16 +1,19 @@
 variable "do_token" {
   description = "DigitalOcean Personal Access Token"
   type        = string
-  sensitive   = true
 }
 
 variable "ssh_public_key" {
-  description = "Your local SSH Public Key"
+  description = "Your local SSH Public Key (e.g., contents of ~/.ssh/id_ed25519.pub)"
   type        = string
 }
 
 variable "tailscale_auth_key" {
-  description = "Tailscale Auth Key"
+  description = "Tailscale Auth Key (One-off or Reusable key from Tailscale Admin Panel)"
   type        = string
-  sensitive   = true
+}
+
+variable "ntfy_topic" {
+  description = "A secret string for your ntfy.sh notifications"
+  type        = string
 }
